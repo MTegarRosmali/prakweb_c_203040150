8f8259dab952d7c0e763b30f3608cb9ab6d83961
MTegarRosmali <02tegarrosmali@gmail.com>
2022-10-10 11:02:35 +0700

    menambahkan shadow di list group

--- mvc/app/views/mahasiswa/index.php
+++ mvc/app/views/mahasiswa/index.php
@@ -4,7 +4,7 @@
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach( $data['mhs'] as $mhs ) : ?>
-                    <li class="list-group-item d-flex justify-content-between align-item-center">
+                    <li class="list-group-item d-flex justify-content-between align-item-center shadow">
                         <?= $mhs['nama']; ?>
                         <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary">detail</a>
                     </li>
